cfbf6af22ae37ee00722f476fb90a1cae705307f
doponalopes <chrisdopona@outlook.com>
2023-07-04 10:09:30 -0300

    feat: Adiciona componente NoRegistry

--- src/components/NoRegistry/index.tsx
+++ src/components/NoRegistry/index.tsx
@@ -1,12 +1,18 @@
-import { VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 
-import Logo from "../../assets/task.file.svg";
+import Logo from "../../assets/task-file.svg";
 
 export function NoRegistry() {
   return (
-    <VStack>
+    <VStack mt={20}>
       <img src={Logo} alt="Your SVG" />
-      <Logo />
+
+      <VStack mt={8} color="gray.400">
+        <Text fontWeight="bold">
+          Você ainda não tem tarefas cadastradas
+        </Text>
+        <Text fontWeight="regular" mt={-2}>Crie tarefas e organize seus itens a fazer</Text>
+      </VStack>
     </VStack>
   )
 }
