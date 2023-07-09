5fef23087c50d59c3cf3036a04c59de3af8884d0
doponalopes <chrisdopona@outlook.com>
2023-07-09 18:26:59 -0300

    refactor: Separa .env

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfZcvMhvIEJw9fnP4xHzKIBXvyhRAKuiU",
-  authDomain: "taskify-dev-99684.firebaseapp.com",
-  projectId: "taskify-dev-99684",
-  storageBucket: "taskify-dev-99684.appspot.com",
-  messagingSenderId: "890345613145",
-  appId: "1:890345613145:web:db8069dc52e5b8f8dbce48",
-  measurementId: "G-L6BWB7SMPF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
